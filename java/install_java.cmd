84c58863ade13f1556723e4855c57e191d0fa3db
TheGitTourist <henrik.schirmacher@outlook.de>
2019-09-19 20:15:36 +0200

    Java 13

--- java/install_java.cmd
+++ java/install_java.cmd
@@ -1,10 +1,15 @@
 @echo off
 chcp 1252 >nul 2>&1
 title Java Installation
-echo Diese Installation muss als Administrator ausgeführt werden!
-echo. 
-pause
-echo.
+net session >nul 2>&1
+if not %errorlevel% == 0 (
+  echo Diese Installation muss als Administrator ausgeführt werden!
+  echo Bitte wie folgt ausführen: Rechtsklick, "Als Administrator ausführen", "Ja"
+  echo.
+  echo Die Installation wird nun aufgrund fehlender Rechte beendet!
+  pause
+  goto END
+)
 echo Java Installation
 echo -----------------
 echo [1/9] Generieren des Installationsskripts
@@ -13,9 +18,10 @@ rem =======================================
 rem ======--- Installationsskript ---======
 echo $root = $PSScriptRoot> %~dp0\%PSDATEI%
 echo $global:ProgressPreference = "SilentlyContinue">> %~dp0\%PSDATEI%
-echo $url = "https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.2%%2B10/OpenJDK12U-jdk_x64_windows_hotspot_12.0.2_10.zip">> %~dp0\%PSDATEI%
-echo $output = "$root\OpenJDK12U-jdk_x64_windows_hotspot_12.0.2_10.zip">> %~dp0\%PSDATEI%
+echo $url = "https://download.java.net/java/GA/jdk13/5b8a42f3905b406298b72d750b6919f6/33/GPL/openjdk-13_windows-x64_bin.zip">> %~dp0\%PSDATEI%
+echo $output = "$root\openjdk-13_windows-x64_bin.zip">> %~dp0\%PSDATEI%
 echo Write-Host "[3/9] Java wird heruntergeladen">> %~dp0\%PSDATEI%
+echo Write-Host "Bitte einige Minuten warten...">> %~dp0\%PSDATEI%
 echo Invoke-WebRequest -Uri $url -OutFile $output>> %~dp0\%PSDATEI%
 echo Write-Host "[3/9] Java wurde heruntergeladen">> %~dp0\%PSDATEI%
 echo Write-Host "[4/9] Altes Java entfernen">> %~dp0\%PSDATEI%
@@ -28,9 +34,15 @@ echo Write-Host "[6/9] Java Archiv entfernen">> %~dp0\%PSDATEI%
 echo Remove-Item -Path "$output" -Force -Recurse -ErrorAction Ignore>> %~dp0\%PSDATEI%
 echo Write-Host "[6/9] Java Archiv wurde entfernt">> %~dp0\%PSDATEI%
 echo Write-Host "[7/9] Umgebungsvariablen setzen">> %~dp0\%PSDATEI%
-echo [Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Java\jdk-12.0.2+10\", "User")>> %~dp0\%PSDATEI%
-echo $userpath = [Environment]::GetEnvironmentVariable("Path", "User")>> %~dp0\%PSDATEI%
-echo [Environment]::SetEnvironmentVariable("Path", $userpath + "C:\Program Files\Java\jdk-12.0.2+10\bin\;", "User")>> %~dp0\%PSDATEI%
+echo [Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Java\jdk-13\", "User")>> %~dp0\%PSDATEI%
+echo $userpath = [Environment]::GetEnvironmentVariable("Path", "User").Split(";")>> %~dp0\%PSDATEI%
+echo $clearpath = "">> %~dp0\%PSDATEI%
+echo for($i=0; $i -lt $userpath.length; $i++){>> %~dp0\%PSDATEI%
+echo if($userpath[$i] -like "*Java*"){>> %~dp0\%PSDATEI%
+echo $clearpath = $clearpath + $userpath[$i] + ";">> %~dp0\%PSDATEI%
+echo }>> %~dp0\%PSDATEI%
+echo }>> %~dp0\%PSDATEI%
+echo [Environment]::SetEnvironmentVariable("Path", $clearpath + "C:\Program Files\Java\jdk-13\bin\;", "User")>> %~dp0\%PSDATEI%
 echo Write-Host "[7/9] Umgebungsvariablen wurden gesetzt">> %~dp0\%PSDATEI%
 rem ======--- Installationsskript ---======
 rem =======================================
@@ -49,3 +61,4 @@ echo ----------------------------
 echo Java ist fertig installiert!
 echo.
 pause
+:END
