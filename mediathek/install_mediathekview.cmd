98a2f939f7c2ac777500e6f3339b00af529e5dac
TheGitTourist <henrik.schirmacher@outlook.de>
2019-09-20 19:47:44 +0200

    MediathekView 13.3.0

--- mediathek/install_mediathekview.cmd
+++ mediathek/install_mediathekview.cmd
@@ -1,10 +1,15 @@
 @echo off
 chcp 1252 >nul 2>&1
 title MediathekView Installation
-echo Diese Installation muss als Administrator ausgeführt werden!
-echo. 
-pause
-echo.
+net session >nul 2>&1
+if not %errorlevel% == 0 (
+  echo Diese Installation muss als Administrator ausgeführt werden!
+  echo Bitte wie folgt ausführen: Rechtsklick, "Als Administrator ausführen", "Ja"
+  echo.
+  echo Die Installation wird nun aufgrund fehlender Rechte beendet!
+  pause
+  goto END
+)
 echo MediathekView Installation
 echo --------------------------
 echo [1/9] Generieren des Installationsskripts
@@ -29,10 +34,15 @@ echo Write-Host "[6/9] MediathekView Archiv entfernen">> %~dp0\%PSDATEI%
 echo Remove-Item -Path "$output" -Force -Recurse -ErrorAction Ignore>> %~dp0\%PSDATEI%
 echo Write-Host "[6/9] MediathekView Archiv wurde entfernt">> %~dp0\%PSDATEI%
 echo Write-Host "[7/9] Verlinkung erstellen">> %~dp0\%PSDATEI%
+setlocal DisableDelayedExpansion
+set "AT=@"
+setlocal EnableDelayedExpansion
 echo $WshShell = New-Object -comObject WScript.Shell>> %~dp0\%PSDATEI%
 echo $Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\MediathekView.lnk")>> %~dp0\%PSDATEI%
-echo $Shortcut.TargetPath = "java.exe">> %~dp0\%PSDATEI%
-echo $Shortcut.Arguments = '-jar "C:\Program Files\MediathekView-13.3.0\MediathekView.jar"'>> %~dp0\%PSDATEI%
+echo $Shortcut.TargetPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe">> %~dp0\%PSDATEI%
+echo $Shortcut.Arguments = %AT%^">> %~dp0\%PSDATEI%
+echo -WindowStyle Hidden ^"java.exe -jar ^'C:\Program Files\MediathekView-13.3.0\MediathekView.jar^'^">> %~dp0\%PSDATEI%
+echo ^"%AT%>> %~dp0\%PSDATEI%
 echo $Shortcut.IconLocation = "C:\Program Files\MediathekView-13.3.0\MediathekView.exe">> %~dp0\%PSDATEI%
 echo $Shortcut.Save()>> %~dp0\%PSDATEI%
 echo Write-Host "[7/9] Verlinkung wurde erstellt">> %~dp0\%PSDATEI%
@@ -53,3 +63,4 @@ echo ----------------------------
 echo MediathekView ist fertig installiert!
 echo.
 pause
+:END
