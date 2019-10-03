ab055ffdfde90f22da374bf68804f9fa69637f4a
TheGitTourist <henrik.schirmacher@outlook.de>
2019-10-03 20:17:20 +0200

    MediathekView 13.5.0

--- mediathek/install_mediathekview.cmd
+++ mediathek/install_mediathekview.cmd
@@ -18,34 +18,21 @@ rem =======================================
 rem ======--- Installationsskript ---======
 echo $root = $PSScriptRoot> %~dp0\%PSDATEI%
 echo $global:ProgressPreference = "SilentlyContinue">> %~dp0\%PSDATEI%
-echo $url = "https://download.mediathekview.de/stabil/MediathekView-13.3.0-win.zip">> %~dp0\%PSDATEI%
-echo $output = "$root\MediathekView-13.3.0-win.zip">> %~dp0\%PSDATEI%
+echo $url = "https://download.mediathekview.de/stabil/MediathekView-13.5.0-win.exe">> %~dp0\%PSDATEI%
+echo $output = "$root\MediathekView-13.5.0-win.exe">> %~dp0\%PSDATEI%
 echo Write-Host "[3/9] MediathekView wird heruntergeladen">> %~dp0\%PSDATEI%
 echo Invoke-WebRequest -Uri $url -OutFile $output>> %~dp0\%PSDATEI%
 echo Write-Host "[3/9] MediathekView wurde heruntergeladen">> %~dp0\%PSDATEI%
 echo Write-Host "[4/9] Altes MediathekView entfernen">> %~dp0\%PSDATEI%
-echo Remove-Item -Path "C:\Program Files\MediathekView-13.2.1" -Force -Recurse -ErrorAction Ignore>> %~dp0\%PSDATEI%
 echo Remove-Item -Path "C:\Program Files\MediathekView-13.3.0" -Force -Recurse -ErrorAction Ignore>> %~dp0\%PSDATEI%
+echo Remove-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\MediathekView.lnk" -Force -Recurse -ErrorAction Ignore>> %~dp0\%PSDATEI%
 echo Write-Host "[4/9] Altes MediathekView wurde entfernt">> %~dp0\%PSDATEI%
-echo Write-Host "[5/9] MediathekView entpacken">> %~dp0\%PSDATEI%
-echo Expand-Archive -Path "$output" -DestinationPath "C:\Program Files\MediathekView-13.3.0\">> %~dp0\%PSDATEI%
-echo Write-Host "[5/9] MediathekView wurde entpackt">> %~dp0\%PSDATEI%
-echo Write-Host "[6/9] MediathekView Archiv entfernen">> %~dp0\%PSDATEI%
+echo Write-Host "[5/9] MediathekView installieren">> %~dp0\%PSDATEI%
+echo Start-Process -Wait -FilePath "$output" -ArgumentList "-q">> %~dp0\%PSDATEI%
+echo Write-Host "[5/9] MediathekView wurde installiert">> %~dp0\%PSDATEI%
+echo Write-Host "[6/9] MediathekView Installer entfernen">> %~dp0\%PSDATEI%
 echo Remove-Item -Path "$output" -Force -Recurse -ErrorAction Ignore>> %~dp0\%PSDATEI%
-echo Write-Host "[6/9] MediathekView Archiv wurde entfernt">> %~dp0\%PSDATEI%
-echo Write-Host "[7/9] Verlinkung erstellen">> %~dp0\%PSDATEI%
-setlocal DisableDelayedExpansion
-set "AT=@"
-setlocal EnableDelayedExpansion
-echo $WshShell = New-Object -comObject WScript.Shell>> %~dp0\%PSDATEI%
-echo $Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\MediathekView.lnk")>> %~dp0\%PSDATEI%
-echo $Shortcut.TargetPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe">> %~dp0\%PSDATEI%
-echo $Shortcut.Arguments = %AT%^">> %~dp0\%PSDATEI%
-echo -WindowStyle Hidden ^"java.exe -jar ^'C:\Program Files\MediathekView-13.3.0\MediathekView.jar^'^">> %~dp0\%PSDATEI%
-echo ^"%AT%>> %~dp0\%PSDATEI%
-echo $Shortcut.IconLocation = "C:\Program Files\MediathekView-13.3.0\MediathekView.exe">> %~dp0\%PSDATEI%
-echo $Shortcut.Save()>> %~dp0\%PSDATEI%
-echo Write-Host "[7/9] Verlinkung wurde erstellt">> %~dp0\%PSDATEI%
+echo Write-Host "[6/9] MediathekView Installer wurde entfernt">> %~dp0\%PSDATEI%
 rem ======--- Installationsskript ---======
 rem =======================================
 echo [1/9] Installationsskript fertig
@@ -61,6 +48,13 @@ powershell.exe Set-ExecutionPolicy Undefined -Scope CurrentUser
 echo [9/9] Ausführungsregel wurde zurückgesetzt
 echo ----------------------------
 echo MediathekView ist fertig installiert!
+echo.
+echo Hinweis: Manuelle Anpassung des Taskleitensymbols notwendig!
+echo.
+echo Bitte manuell das MediathekView-Symbol in der Taskleiste entfernen
+echo (Rechtsklick, "Von Taskleiste lösen") und
+echo aus dem Startmenü das "MediathekView"-Symbol (nicht "MediathekView IPv4"!)
+echo aus dem Ordner "MediathekView" auf die Taskleiste ziehen.
 echo.
 pause
 :END
